5a3880a7df0f668cbbf1a51f66d32622b08cf9b2
Ricki Ajirasman <rickicode@gmail.com>
2017-10-13 01:46:31 +0700

    New Update Again

---
+++ protected/app/base/logs/log-2017-10-13.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2017-10-13 01:43:32 --> UTF-8 Support Enabled
+DEBUG - 2017-10-13 01:43:33 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-10-13 01:43:35 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2017-10-13 01:43:35 --> Config file loaded: E:\www\htdocs\bogasoftware\bogapos\protected\app\base\config/ion_auth.php
+ERROR - 2017-10-13 01:43:37 --> Cache: Failed to initialize APC; extension not loaded/enabled?
+DEBUG - 2017-10-13 01:43:37 --> Cache adapter "apc" is unavailable. Falling back to "file" backup adapter.
+DEBUG - 2017-10-13 01:43:37 --> File loaded: protected/app/modules/home/models/Home_model.php
+DEBUG - 2017-10-13 01:43:37 --> File loaded: protected/app/modules/reports/models/Profit_loss_model.php
+DEBUG - 2017-10-13 01:43:38 --> File loaded: protected/app/modules/home/views/home.php
+ERROR - 2017-10-13 01:43:38 --> Could not find the language line "menu_categories_label"
+DEBUG - 2017-10-13 01:43:38 --> File loaded: E:\www\htdocs\bogasoftware\bogapos\protected\app\views\themes/default.php
+DEBUG - 2017-10-13 01:43:38 --> Total execution time: 6.9933
+DEBUG - 2017-10-13 01:43:44 --> UTF-8 Support Enabled
+DEBUG - 2017-10-13 01:43:44 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-10-13 01:43:44 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2017-10-13 01:43:44 --> Config file loaded: E:\www\htdocs\bogasoftware\bogapos\protected\app\base\config/ion_auth.php
+ERROR - 2017-10-13 01:43:44 --> Cache: Failed to initialize APC; extension not loaded/enabled?
+DEBUG - 2017-10-13 01:43:44 --> Cache adapter "apc" is unavailable. Falling back to "file" backup adapter.
+DEBUG - 2017-10-13 01:43:44 --> File loaded: protected/app/modules/home/models/Home_model.php
+DEBUG - 2017-10-13 01:43:44 --> File loaded: protected/app/modules/reports/models/Profit_loss_model.php
+DEBUG - 2017-10-13 01:43:44 --> Total execution time: 0.6894
